d111c4310a036633ffc68170b57eb3aa7cf91dd4
Rene Cuchillas <31251405+r2c2@users.noreply.github.com>
2022-03-10 18:45:42 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,9 +6,10 @@ class minecraft {
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+    before => Service['minecraft'],
   }
   
-  package {'java':
+  package {'java-11-openjdk.x86_64':
     ensure => present,
   }
   
