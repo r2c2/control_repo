c09d0024f0da39fcffcfa19d0a10255de1797ee5
Rene Cuchillas <31251405+r2c2@users.noreply.github.com>
2022-03-10 18:41:19 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,8 +4,8 @@ class minecraft {
   }
   
   file {'/opt/minecraft/minecraft_server.jar':
-    ensure => file
-    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
   
   package {'java':
